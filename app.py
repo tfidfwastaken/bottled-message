e350bedd167a4b69c71480564d0639cb52b2c246
Atharva Raykar <raykar.ath@gmail.com>
2019-06-30 23:10:11 +0530

    Add modal

--- app.py
+++ app.py
@@ -4,12 +4,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", message="Message Appears Here")
 
 @app.route("/submit", methods=["POST"])
 def submit():
+    confirmed = True
     text = request.form.get("bottletext")
-    return text
+    if text == "":
+        confirmed = False
+    return render_template("index.html", confirmed=confirmed)
+
+@app.route("/retrieve")
+def retrieve():
+    message="ayo"
+    return render_template("index.html", message=message)
 
 if __name__ == "__main__":
     app.run(debug=True)
