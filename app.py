d6c7ef8456dbf2c25f57784daa0020303090b662
Atharva Raykar <raykar.ath@gmail.com>
2019-07-01 20:08:45 +0530

    Actually do what it says (kinda)

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
+import random
 
 app = Flask(__name__)
+
+messages = []
 
 @app.route("/")
 def index():
@@ -10,13 +13,17 @@ def index():
 def submit():
     confirmed = True
     text = request.form.get("bottletext")
+    messages.append(text)
     if text == "":
         confirmed = False
     return render_template("index.html", confirmed=confirmed)
 
 @app.route("/retrieve")
 def retrieve():
-    message="ayo"
+    message = "No messages in the sea :( ...Maybe you can add one?"
+    if messages != []:
+        message = random.choice(messages)
+        messages.remove(message)
     return render_template("index.html", message=message)
 
 if __name__ == "__main__":
